85d93f44cae20260f20c630eae8d18f81e8e005f
loonskai <loonskai@gmail.com>
2024-12-29 20:44:51 +0100

    Add Input type

--- src/eva.rs
+++ src/eva.rs
@@ -1,20 +1,17 @@
-use std::fmt::{Debug, Display};
+use std::fmt::{Debug};
 use unicode_segmentation::{self, UnicodeSegmentation};
+
+enum Input {
+  Number(isize),
+  String(String),
+  Add(char, isize, isize),
+}
 
 #[derive(PartialEq)]
 #[derive(Debug)]
 enum ExpressionType {
   Int(isize),
   Str(String)
-}
-
-impl Display for ExpressionType {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      match self {
-          ExpressionType::Int(num) => write!(f, "{num}"),
-          ExpressionType::Str(s) => write!(f, "{s}"),
-      }
-  }
 }
 
 pub struct Eva {}
@@ -24,18 +21,26 @@ impl Eva {
     Eva {}
   }
 
-  fn eval(&self, exp: String) -> Option<ExpressionType> {
-    if let Ok(num) = exp.parse::<isize>() {
-      return Some(ExpressionType::Int(num));
+  fn eval(&self, exp: Input) -> Option<ExpressionType> {
+    match exp {
+        Input::Add(operator, op1,op2 ) => {
+          if operator == '+' {
+            return Some(ExpressionType::Int(op1 + op2));
+          } else {
+            return None;
+          }
+        }
+        Input::Number(n) => {
+          return Some(ExpressionType::Int(n));
+        },
+        Input::String(s) => {
+          if s.graphemes(true).nth(0).unwrap() == r#"""# && s.graphemes(true).last().unwrap() == r#"""# {
+            let with_quotation_trimmed = s[1..s.len()-1].to_string();
+            return Some(ExpressionType::Str(with_quotation_trimmed));
+          }
+          return None
+        },
     }
-
-    if exp.graphemes(true).nth(0).unwrap() == r#"""# && 
-      exp.graphemes(true).last().unwrap() == r#"""# {
-        let with_quotation_trimmed = exp[1..exp.len()-1].to_string();
-      return Some(ExpressionType::Str(with_quotation_trimmed));
-    }
-
-    return None;
   }
 }
 
@@ -48,10 +53,10 @@ mod tests {
   fn self_evaluating_expressions() {
     let eva = Eva::new();
 
-    assert_eq!(eva.eval("1".to_string()), Some(ExpressionType::Int(1)));
-    assert_eq!(eva.eval("-10".to_string()), Some(ExpressionType::Int(-10)));
+    assert_eq!(eva.eval(Input::Number(1)), Some(ExpressionType::Int(1)));
+    assert_eq!(eva.eval(Input::Number(-10)), Some(ExpressionType::Int(-10)));
 
-    assert_eq!(eva.eval(r#""hello""#.to_string().to_string()), Some(ExpressionType::Str("hello".to_string())));
-    assert_eq!(eva.eval(r#""🎅 新年快乐!""#.to_string()), Some(ExpressionType::Str("🎅 新年快乐!".to_string())));
+    assert_eq!(eva.eval(Input::String(r#""hello""#.to_string())), Some(ExpressionType::Str("hello".to_string())));
+    assert_eq!(eva.eval(Input::String(r#""🎅 新年快乐!""#.to_string())), Some(ExpressionType::Str("🎅 新年快乐!".to_string())));
   }
 }
