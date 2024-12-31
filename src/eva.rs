4ec49b8deac02815ea54209277834d8002face05
loonskai <loonskai@gmail.com>
2024-12-31 18:31:30 +0100

    Addition operation

--- src/eva.rs
+++ src/eva.rs
@@ -1,17 +1,12 @@
 use std::fmt::{Debug};
 use unicode_segmentation::{self, UnicodeSegmentation};
 
-enum Input {
-  Number(isize),
-  String(String),
-  Add(char, isize, isize),
-}
-
 #[derive(PartialEq)]
 #[derive(Debug)]
-enum ExpressionType {
+enum Exp {
   Int(isize),
-  Str(String)
+  Str(String),
+  Operation(char, Box<Exp>, Box<Exp>),
 }
 
 pub struct Eva {}
@@ -21,22 +16,28 @@ impl Eva {
     Eva {}
   }
 
-  fn eval(&self, exp: Input) -> Option<ExpressionType> {
+  fn eval(&self, exp: Exp) -> Option<Exp> {
     match exp {
-        Input::Add(operator, op1,op2 ) => {
-          if operator == '+' {
-            return Some(ExpressionType::Int(op1 + op2));
-          } else {
-            return None;
+        Exp::Operation(operator, op1,op2 ) => {
+          match operator {
+            '+' => {
+              let n1 = self.eval(*op1).expect("Invalid operand.");
+              let n2 = self.eval(*op2).expect("Invalid operand.");
+              match (n1, n2) {
+                (Exp::Int(num1), Exp::Int(num2)) => Some(Exp::Int(num1 + num2)),
+                _ => None
+              }
+            },
+            _ => None
           }
         }
-        Input::Number(n) => {
-          return Some(ExpressionType::Int(n));
+        Exp::Int(n) => {
+          return Some(Exp::Int(n));
         },
-        Input::String(s) => {
+        Exp::Str(s) => {
           if s.graphemes(true).nth(0).unwrap() == r#"""# && s.graphemes(true).last().unwrap() == r#"""# {
             let with_quotation_trimmed = s[1..s.len()-1].to_string();
-            return Some(ExpressionType::Str(with_quotation_trimmed));
+            return Some(Exp::Str(with_quotation_trimmed));
           }
           return None
         },
@@ -53,10 +54,27 @@ mod tests {
   fn self_evaluating_expressions() {
     let eva = Eva::new();
 
-    assert_eq!(eva.eval(Input::Number(1)), Some(ExpressionType::Int(1)));
-    assert_eq!(eva.eval(Input::Number(-10)), Some(ExpressionType::Int(-10)));
+    assert_eq!(eva.eval(Exp::Int(1)), Some(Exp::Int(1)));
+    assert_eq!(eva.eval(Exp::Int(-10)), Some(Exp::Int(-10)));
 
-    assert_eq!(eva.eval(Input::String(r#""hello""#.to_string())), Some(ExpressionType::Str("hello".to_string())));
-    assert_eq!(eva.eval(Input::String(r#""🎅 新年快乐!""#.to_string())), Some(ExpressionType::Str("🎅 新年快乐!".to_string())));
+    assert_eq!(eva.eval(Exp::Str(r#""hello""#.to_string())), Some(Exp::Str("hello".to_string())));
+    assert_eq!(eva.eval(Exp::Str(r#""🎅 新年快乐!""#.to_string())), Some(Exp::Str("🎅 新年快乐!".to_string())));
+  }
+
+  #[test]
+  fn addition() {
+    let eva = Eva::new();
+
+    assert_eq!(eva.eval(
+      Exp::Operation('+', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
+      Some(Exp::Int(5))
+    );
+    assert_eq!(eva.eval(
+      Exp::Operation(
+        '+', 
+        Box::new(Exp::Operation('+', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
+        Box::new( Exp::Operation('+', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))),
+      ),
+    ), Some(Exp::Int(10)));
   }
 }
