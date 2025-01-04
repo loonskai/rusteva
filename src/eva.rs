f4e15d5bd4c7a4157b6d15db955e8f40bfc4ea68
loonskai <loonskai@gmail.com>
2025-01-04 07:39:03 +0100

    Difine Value enum

--- src/eva.rs
+++ src/eva.rs
@@ -1,12 +1,19 @@
-use std::fmt::{Debug};
+use std::{f32::consts::E, fmt::Debug};
 use unicode_segmentation::{self, UnicodeSegmentation};
 
 #[derive(PartialEq)]
 #[derive(Debug)]
-enum Exp {
+pub enum Value {
   Int(isize),
-  Str(String),
-  Operation(char, Box<Exp>, Box<Exp>),
+  Str(String)
+}
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+enum Expr {
+  Literal(Value),
+  Operation(char, Box<Expr>, Box<Expr>),
+  VariableDeclaration(String, String, Value)
 }
 
 pub struct Eva {}
@@ -16,39 +23,50 @@ impl Eva {
     Eva {}
   }
 
-  fn eval(&self, exp: Exp) -> Option<Exp> {
+  fn eval(&self, exp: Expr) -> Option<Value> {
     match exp {
-        Exp::Operation(operator, op1,op2 ) => {
+        Expr::Literal(value) => {
+          match value {
+            Value::Int(n) => {
+              return Some(Value::Int(n));
+            },
+            Value::Str(s) => {
+              if s.graphemes(true).nth(0).unwrap() == r#"""# && s.graphemes(true).last().unwrap() == r#"""# {
+                let with_quotation_trimmed = s[1..s.len()-1].to_string();
+                return Some(Value::Str(with_quotation_trimmed));
+              }
+              return None
+            },
+          }
+        }
+        Expr::Operation(operator, exp1,exp2 ) => {
           match operator {
             '+' => {
-              let n1 = self.eval(*op1).expect("Invalid operand.");
-              let n2 = self.eval(*op2).expect("Invalid operand.");
+              // Q: How to reuse the parts from each arm?
+              let n1 = self.eval(*exp1).expect("Invalid operand.");
+              let n2 = self.eval(*exp2).expect("Invalid operand.");
               match (n1, n2) {
-                (Exp::Int(num1), Exp::Int(num2)) => Some(Exp::Int(num1 + num2)),
+                (Value::Int(num1), Value::Int(num2)) => Some(Value::Int(num1 + num2)),
                 _ => None
               }
             },
             '*' => {
-              let n1 = self.eval(*op1).expect("Invalid operand.");
-              let n2 = self.eval(*op2).expect("Invalid operand.");
+              let n1 = self.eval(*exp1).expect("Invalid operand.");
+              let n2 = self.eval(*exp2).expect("Invalid operand.");
               match (n1, n2) {
-                (Exp::Int(num1), Exp::Int(num2)) => Some(Exp::Int(num1 * num2)),
+                (Value::Int(num1), Value::Int(num2)) => Some(Value::Int(num1 * num2)),
                 _ => None
               }
             },
             _ => None
           }
         }
-        Exp::Int(n) => {
-          return Some(Exp::Int(n));
-        },
-        Exp::Str(s) => {
-          if s.graphemes(true).nth(0).unwrap() == r#"""# && s.graphemes(true).last().unwrap() == r#"""# {
-            let with_quotation_trimmed = s[1..s.len()-1].to_string();
-            return Some(Exp::Str(with_quotation_trimmed));
+        Expr::VariableDeclaration(var_keyword, var_name, value) => {
+          if var_keyword != "var".to_string() {
+            panic!("Unknown keyword: {}", var_keyword);
           }
-          return None
-        },
+          return Some(value)
+        }
     }
   }
 }
@@ -62,11 +80,11 @@ mod tests {
   fn self_evaluating_expressions() {
     let eva = Eva::new();
 
-    assert_eq!(eva.eval(Exp::Int(1)), Some(Exp::Int(1)));
-    assert_eq!(eva.eval(Exp::Int(-10)), Some(Exp::Int(-10)));
+    assert_eq!(eva.eval(Expr::Literal(Value::Int(1))), Some(Value::Int(1)));
+    assert_eq!(eva.eval(Expr::Literal(Value::Int(-10))), Some(Value::Int(-10)));
 
-    assert_eq!(eva.eval(Exp::Str(r#""hello""#.to_string())), Some(Exp::Str("hello".to_string())));
-    assert_eq!(eva.eval(Exp::Str(r#""🎅 新年快乐!""#.to_string())), Some(Exp::Str("🎅 新年快乐!".to_string())));
+    assert_eq!(eva.eval(Expr::Literal(Value::Str(r#""hello""#.to_string()))), Some(Value::Str("hello".to_string())));
+    assert_eq!(eva.eval(Expr::Literal(Value::Str(r#""🎅 新年快乐!""#.to_string()))), Some(Value::Str("🎅 新年快乐!".to_string())));
   }
 
   #[test]
@@ -74,23 +92,23 @@ mod tests {
     let eva = Eva::new();
 
     assert_eq!(eva.eval(
-      Exp::Operation('+', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
-      Some(Exp::Int(5))
+      Expr::Operation('+', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))), 
+      Some(Value::Int(5))
     );
     assert_eq!(eva.eval(
-      Exp::Operation(
+      Expr::Operation(
         '+', 
-        Box::new(Exp::Operation('+', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
-        Box::new(Exp::Int(3)),
+        Box::new(Expr::Operation('+', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))), 
+        Box::new(Expr::Literal(Value::Int(3))),
       ),
-    ), Some(Exp::Int(8)));
+    ), Some(Value::Int(8)));
     assert_eq!(eva.eval(
-      Exp::Operation(
+      Expr::Operation(
         '+', 
-        Box::new(Exp::Operation('+', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
-        Box::new(Exp::Operation('+', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))),
+        Box::new(Expr::Operation('+', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))), 
+        Box::new(Expr::Operation('+', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))),
       ),
-    ), Some(Exp::Int(10)));
+    ), Some(Value::Int(10)));
   }
 
   #[test]
@@ -98,22 +116,29 @@ mod tests {
     let eva = Eva::new();
 
     assert_eq!(eva.eval(
-      Exp::Operation('*', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
-      Some(Exp::Int(6))
+      Expr::Operation('*', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))), 
+      Some(Value::Int(6))
     );
     assert_eq!(eva.eval(
-      Exp::Operation(
+      Expr::Operation(
         '*', 
-        Box::new(Exp::Operation('*', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
-        Box::new(Exp::Int(3)),
+        Box::new(Expr::Operation('*', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))), 
+        Box::new(Expr::Literal(Value::Int(3))),
       ),
-    ), Some(Exp::Int(18)));
+    ), Some(Value::Int(18)));
     assert_eq!(eva.eval(
-      Exp::Operation(
+      Expr::Operation(
         '*', 
-        Box::new(Exp::Operation('*', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))), 
-        Box::new(Exp::Operation('*', Box::new(Exp::Int(3)), Box::new(Exp::Int(2)))),
+        Box::new(Expr::Operation('*', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))), 
+        Box::new(Expr::Operation('*', Box::new(Expr::Literal(Value::Int(3))), Box::new(Expr::Literal(Value::Int(2))))),
       ),
-    ), Some(Exp::Int(36)));
+    ), Some(Value::Int(36)));
+  }
+
+  #[test]
+  fn variable_declaration() {
+    let eva = Eva::new();
+
+    assert_eq!(eva.eval(Expr::VariableDeclaration("var".to_string(), "x".to_string(), Value::Int(10))), Some(Value::Int(10)))
   }
 }
